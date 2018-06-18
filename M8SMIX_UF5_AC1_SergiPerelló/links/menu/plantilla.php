84f6811cad2caca2758bc0e8b1145a44e182dc05
SergiPerello <sergimine@gmail.com>
2018-06-18 14:04:01 +0200

    Añadidos todos los proyectos de HTML

---
+++ M8SMIX_UF5_AC1_SergiPerelló/links/menu/plantilla.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Plantilla</title>
+	<link rel="icon" type="image/x-icon" href="../images/favicon.png">
+</head>
+<body>
+<h1>Plantilla</h1>
+<a href="../../jugador.html">VOLVER</a>
+</body>
+</html>
---
+++ M8SMIX_UF5_AC1_SergiPerelló/links/user.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Usuario</title>
+	<link rel="icon" type="image/x-icon" href="../images/favicon.png">
+</head>
+<body>
+<h1>Usuario</h1>
+<a href="../jugador.html">VOLVER</a>
+</body>
+</html>
